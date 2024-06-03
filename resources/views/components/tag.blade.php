e16d36c2fdf21bb893439ffdfe8f0ccc8cc47672
MusingaBrian <musingabrians@gmail.com>
2024-06-03 20:03:55 +0300

    Refactor the Job tags

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,14 @@
-<a href="" class="bg-white/15 hover:bg-white/35 px-2 py-2 rounded-xl text-2xs transition-colors-300 duration-300 font-bold px-3">{{ $slot }}</a>
+@props(['size' => 'base'])
+
+@php
+    $classes = "bg-white/15 hover:bg-white/35 rounded-xl transition-colors-300 duration-300 font-bold px-3";
+
+    if($size === 'base'){
+        $classes .= ' px-5 py-1 text-sm';
+    }
+    if($size === 'small'){
+        $classes .= ' px-2 py-1 text-2sm'; 
+        
+    }
+@endphp
+<a href="" class="{{ $classes }}"  }}>{{ $slot }}</a>
