4a999d256d0343667dab5d36562f92154525052d
MusingaBrian <musingabrians@gmail.com>
2024-06-03 20:04:52 +0300

    Refactor simillar tailwind class

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,15 +1,15 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-600 group transition-colors duration-300">
+<x-panel class="flex-col text-center">
     <div class="self-start text-sm">Laracast</div>
-    <div class="py-8">
+    <div class="py-8 ">
         <h3 class=" group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Video Editor</h3>
         <p class="text-sm mt-4">Full time - 500,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
-            <x-tag>Tag</x-tag>
+            <x-tag size='small'>Tag</x-tag>
+            <x-tag size='small'>Tag</x-tag>
+            <x-tag size='small'>Tag</x-tag> 
         </div>
         <x-employer-logo :width="42"></x-employer-logo>
     </div>
-</div>
+</x-panel>
