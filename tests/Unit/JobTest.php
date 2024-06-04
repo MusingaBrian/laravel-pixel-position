b6bafd33850efb3768db6ac086f11fd11a937910
MusingaBrian <musingabrians@gmail.com>
2024-06-04 12:20:46 +0300

    Add Relationship test

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,14 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Models\Employer;
+use App\Models\Job;
+
+test('It belongs to a employer', function () {
+
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
 });
